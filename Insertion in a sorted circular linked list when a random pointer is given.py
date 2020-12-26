c52b6db0c4c3fc47c04a8a116a81e336dc0bfbff
Souhardya Chakraborty <64438285+souhardya1@users.noreply.github.com>
2020-12-27 00:39:49 +0530

    Add files via upload

---
+++ Insertion in a sorted circular linked list when a random pointer is given.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  1 13:12:45 2020
+
+@author: Souhardya
+"""
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class LinkedList:
+    def __init__(self):
+        self.head=None
+    def push(self,new_data):
+        new_node=Node(new_data)
+        new_node.next=self.head
+        curr=self.head
+        if self.head!=None:
+            while curr.next!=self.head:
+                curr=curr.next
+            curr.next=new_node
+        else:
+            new_node.next=new_node
+        self.head=new_node
+    def printList(self):
+        curr=self.head
+        while True:
+            print(curr.data,end=' ')
+            curr=curr.next
+            if curr==self.head:
+                break
+ll=LinkedList()
+arr=[12, 56, 2, 11, 1, 90]
+for i in sorted(arr,reverse=True):
+    ll.push(i)
+ll.printList()
